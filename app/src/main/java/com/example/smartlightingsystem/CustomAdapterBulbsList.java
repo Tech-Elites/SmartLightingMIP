4ffb3bafcd54e8d2a22207bb4cf2445728bd45ff
harshva89 <harshvaswani89@gmail.com>
2021-05-26 17:55:51 +0530

    bulb yellow color

--- app/src/main/java/com/example/smartlightingsystem/CustomAdapterBulbsList.java
+++ app/src/main/java/com/example/smartlightingsystem/CustomAdapterBulbsList.java
@@ -1,10 +1,13 @@
 package com.example.smartlightingsystem;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -31,14 +34,20 @@ public class CustomAdapterBulbsList extends ArrayAdapter<BulbsInfo> {
 
         try{
             BulbsInfo b = getItem(position);
+            ImageView imgView = currentItemView.findViewById(R.id.bulbImageView);
             TextView tname = currentItemView.findViewById(R.id.eachRoomLightName);
             TextView tstatus = currentItemView.findViewById(R.id.eachRoomLightStatus);
             tname.setText(b.getName());
             if(b.getStatus()==1){
                 tstatus.setText("ON");
+                tstatus.setTextColor(Color.parseColor("#FFEC00"));
+                tname.setTextColor(Color.parseColor("#FFEC00"));
+                imgView.setBackgroundResource(R.drawable.torchyellow);
             }
             else{
                 tstatus.setText("OFF");
+                tstatus.setTextColor(Color.parseColor("#FFFFFF"));
+                tname.setTextColor(Color.parseColor("#FFFFFF"));
             }
         }catch (Exception e){
             System.out.println(e);
